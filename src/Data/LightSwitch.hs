e9e8854851c79cffca82c33bf0968be6e00f60cf
sebi <sebi@hyaena.Sahara>
2016-02-28 02:55:46 +0100

    Code cleanup (scan+hindent).
    Cleanup was made with shortcut for that: make scan hindent.

--- src/Data/LightSwitch.hs
+++ src/Data/LightSwitch.hs
@@ -1,23 +1,18 @@
-module Data.LightSwitch ( 
+module Data.LightSwitch (LightSwitch(..), getDefaultLightSwitch, switch) where
 
-    LightSwitch(..)
+import           Control.Monad.State
 
-    , getDefaultLightSwitch
-    , switch 
-
-) where
-
-import Control.Monad.State
-
-data LightSwitch      =  SwitchClosed | SwitchOpened 
-    deriving ( Eq, Show )
+data LightSwitch = SwitchClosed
+                 | SwitchOpened
+  deriving (Eq, Show)
 
 getDefaultLightSwitch :: State LightSwitch ()
-getDefaultLightSwitch =  do put SwitchClosed
+getDefaultLightSwitch = do
+  put SwitchClosed
 
 switch :: State LightSwitch ()
-switch                =  do 
-                           lightSwitch <- get
-                           case lightSwitch of
-                               SwitchClosed -> put SwitchOpened
-                               SwitchOpened -> put SwitchClosed
+switch = do
+  lightSwitch <- get
+  case lightSwitch of
+    SwitchClosed -> put SwitchOpened
+    SwitchOpened -> put SwitchClosed
--- test/Data/LightSwitchSpec.hs
+++ test/Data/LightSwitchSpec.hs
@@ -1,25 +1,24 @@
 module Data.LightSwitchSpec (spec) where
 
-import Test.Hspec
-import Data.LightSwitch 
-import Control.Monad.State
+import           Test.Hspec
+import           Data.LightSwitch
+import           Control.Monad.State
 
 spec :: Spec
-spec =  do
+spec = do
+  describe "getDefaultLightSwitch" $ do
+    it "always returns closed switch" $ do
+      execState getDefaultLightSwitch SwitchClosed `shouldBe` SwitchClosed
+      execState getDefaultLightSwitch SwitchOpened `shouldBe` SwitchClosed
 
-    describe "getDefaultLightSwitch" $ do
-        it "always returns closed switch" $ do
-            execState getDefaultLightSwitch SwitchClosed  `shouldBe` SwitchClosed
-            execState getDefaultLightSwitch SwitchOpened  `shouldBe` SwitchClosed
+  describe "when defaultLightSwitch switches" $ do
+    it "returns opened switch" $ do
+      execState switch (execState getDefaultLightSwitch SwitchOpened) `shouldBe` SwitchOpened
 
-    describe "when defaultLightSwitch switches" $ do
-        it "returns opened switch" $ do
-            execState switch ( execState getDefaultLightSwitch SwitchOpened )  `shouldBe` SwitchOpened
+  describe "when closed switch switches" $ do
+    it "returns opened switch" $ do
+      execState switch SwitchClosed `shouldBe` SwitchOpened
 
-    describe "when closed switch switches" $ do
-        it "returns opened switch" $ do
-            execState switch SwitchClosed `shouldBe` SwitchOpened
-
-    describe "when opened switch switches" $ do
-        it "returns closed switch" $ do
-            execState switch SwitchOpened `shouldBe` SwitchClosed
+  describe "when opened switch switches" $ do
+    it "returns closed switch" $ do
+      execState switch SwitchOpened `shouldBe` SwitchClosed
